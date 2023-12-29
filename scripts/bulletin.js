ccfd15f0c83c896e903d56256c56d37ca5422f17
stltcb <127578679+stltcb@users.noreply.github.com>
2023-12-29 14:36:50 -0600

    Update bulletin.js

--- scripts/bulletin.js
+++ scripts/bulletin.js
@@ -416,3 +416,20 @@
             body.appendChild(viewSection);
         }
     }
+
+    // Function to scroll to the top of the page
+    function goToTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // For smooth scrolling
+      });
+    }
+    
+    function scrollFunction() {
+      var btn = document.getElementById("goTopBtn");
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btn.style.display = "block";
+      } else {
+        btn.style.display = "none";
+      }
+    }
